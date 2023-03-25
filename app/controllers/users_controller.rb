24c45a1630d13f1210010c07021ae54cd7d0bf4f
SusanGithaigaN <susan.githaiga@student.moringaschool.com>
2023-03-25 05:31:06 +0300

    Testing Auto Login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,9 +11,23 @@ class UsersController < ApplicationController
         end        
     end
 
+    # auto login feature
+    def show
+        # user = User.find(user_params)
+        if current_user
+            render json: current_user
+        else
+            render json: { error: "Not authorized" }, status: :unauthorized
+        end        
+    end
+
     private
 
     def user_params
         params.permit(:username, :password, :password_confirmation, :image_url, :bio)
+    end
+
+    def current_user
+        @current_user ||= User.find_by(id: session[:user_id])
     end
 end
